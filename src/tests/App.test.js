1497a28ec5db74ea5cc328758e9e31c5e093c983
Camilla Geber <camilla.geber@gmail.com>
2022-07-19 11:30:02 -0300

    Adiciona e modifica o requisito 5

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,7 +3,9 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 
+
 describe('Testes da aplicação StarWars', () => {
+
   test('Verifica se há um input de pesquisa por nome na tela', () => {
     render(<App />);
     const inputName = screen.getByTestId(/name-filter/i);
@@ -49,34 +51,76 @@ test('Verifica se a lista é filtrada ao digitar no input de pesquisa por nome',
   expect(teste1).toBeInTheDocument();
   expect(teste2).toBeInTheDocument();
 });
-test('verifica se o campo de filtragem começa com os valores: "population, maior que"', () => {
+
+test('Verifica os valores iniciais dos filtros', () => {
   render(<App />);
   const population = screen.getByText('population');
-  const greaterThen = screen.getByText('maior que');
+  const greaterThan = screen.getByText('maior que');
   expect(population).toBeInTheDocument();
-  expect(greaterThen).toBeInTheDocument();
-})
+  expect(greaterThan).toBeInTheDocument();
+});
 
-
-test('Testa as opções do dropdown de comparação', async () => {
+ test('Verifica se o filtro "comparison" tem a opção "Maior que"', async () => {
   render(<App />);
   const inputColumn = screen.getByTestId(/column-filter/i)
   const option1 = screen.getByRole('option', { name: 'diameter' })
   userEvent.selectOptions(inputColumn, option1)
-  
-  const option2 = screen.getByRole('option', { name: 'population' })
-  userEvent.selectOptions(inputColumn, option2)
-  
-  const inputComparison1 = screen.getByTestId(/comparison-filter/i)
-  userEvent.selectOptions(inputComparison1, screen.getByRole('option', { name: 'maior que' }))
 
-  const inputComparison2 = screen.getByTestId(/comparison-filter/i)
-  userEvent.selectOptions(inputComparison2, screen.getByRole('option', { name: 'menor que' }))
-  
+  const inputComparison1 = screen.getByTestId(/comparison-filter/i)
+  const greaterThan = screen.getByRole('option', { name: 'maior que' })
+  userEvent.selectOptions(inputComparison1, greaterThan)
+
   const inputValue = screen.getByTestId(/value-filter/i);
+  const buttonFilter = screen.getByRole('button', {  name: /filtrar/i})
   expect(inputValue).toHaveAttribute("type", "number");
   userEvent.type(inputValue, "20000");
-  userEvent.click(screen.getByRole('button', {  name: /filtrar/i}));
+  userEvent.click(buttonFilter);
+ 
+  const response = await screen.findAllByRole('cell');
+  expect(response).toHaveLength(130);  
 });
+
+test('Verifica se o filtro "comparison" tem a opção "Menor que"', async () => {
+  render(<App />);
+  const inputColumn = screen.getByTestId(/column-filter/i)
+  const option1 = screen.getByRole('option', { name: 'diameter' })
+  userEvent.selectOptions(inputColumn, option1)
+
+  const inputComparison1 = screen.getByTestId(/comparison-filter/i)
+  const greaterThan = screen.getByRole('option', { name: 'menor que' })
+  userEvent.selectOptions(inputComparison1, greaterThan)
+
+  const inputValue = screen.getByTestId(/value-filter/i);
+  const buttonFilter = screen.getByRole('button', {  name: /filtrar/i})
+  expect(inputValue).toHaveAttribute("type", "number");
+  userEvent.type(inputValue, "20000");
+  userEvent.click(buttonFilter);
+ 
+  const response = await screen.findAllByRole('cell');
+  expect(response).toHaveLength(130);
 });
+
+test('Verifica se o filtro "comparison" tem a opção "Igual a"', async () => {
+  render(<App />);
+  const inputColumn = screen.getByTestId(/column-filter/i)
+  const option1 = screen.getByRole('option', { name: 'diameter' })
+  userEvent.selectOptions(inputColumn, option1)
+
+  const inputComparison1 = screen.getByTestId(/comparison-filter/i)
+  const greaterThan = screen.getByRole('option', { name: 'igual a' })
+  userEvent.selectOptions(inputComparison1, greaterThan)
+
+  const inputValue = screen.getByTestId(/value-filter/i);
+  const buttonFilter = screen.getByRole('button', {  name: /filtrar/i})
+  expect(inputValue).toHaveAttribute("type", "number");
+  userEvent.type(inputValue, "20000");
+  userEvent.click(buttonFilter);
+ 
+  const response = await screen.findAllByRole('cell');
+  expect(response).toHaveLength(130);
+});
+
+});
+
 // fonte: https://github.com/testing-library/user-event/issues/358
+
